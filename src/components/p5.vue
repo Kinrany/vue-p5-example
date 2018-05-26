d30f00b7a2c5647fa86d4b6b7c48f8426f71812c
Ruslan <kinrany@yandex.com>
2018-05-26 22:09:56 +0300

    Add mouse events

--- src/components/p5.vue
+++ src/components/p5.vue
@@ -21,19 +21,44 @@ export default {
   data() {
     return { sketch: null };
   },
+  methods: {
+    filterByKeys(obj, keys) {
+      return keys.reduce(
+        (acc, key) => Object.assign(acc, { [key]: obj[key] }),
+        {}
+      );
+    }
+  },
   mounted() {
-    new p5(sketch => {
-      sketch.setup = () => this.setup(sketch);
-      sketch.draw = () => {
-        this.$emit("update");
-        this.draw(sketch, this.state);
-      };
+    new p5(
+      sketch =>
+        Object.assign(sketch, {
+          setup: () => this.setup(sketch),
+          draw: () => {
+            this.$emit("update");
+            this.draw(sketch, this.state);
+          },
 
-      sketch.keyPressed = () => this.$emit("key-pressed", sketch.keyCode);
-      sketch.keyReleased = () => this.$emit("key-released", sketch.keyCode);
-      sketch.keyTyped = () => this.$emit("key-typed", sketch.key);
+          keyPressed: () => this.$emit("key-pressed", sketch.keyCode),
+          keyReleased: () => this.$emit("key-released", sketch.keyCode),
+          keyTyped: () => this.$emit("key-typed", sketch.key),
 
-    }, "p5-container");
+          mousePressed: () => this.$emit("mouse-pressed", sketch.mouseButton),
+          mouseReleased: () => this.$emit("mouse-released", sketch.mouseButton),
+          mouseClicked: () => this.$emit("mouse-clicked", sketch.mouseButton),
+          mouseMoved: () =>
+            this.$emit(
+              "mouse-moved",
+              this.filterByKeys(sketch, [
+                "mouseX",
+                "mouseY",
+                "pmouseX",
+                "pmouseY"
+              ])
+            )
+        }),
+      "p5-container"
+    );
   }
 };
 </script>
