2a25e6116a59f19579fb12f48d59b4a8ee9a6e66
Ruslan <kinrany@yandex.com>
2018-05-26 14:10:17 +0300

    Add empty p5 component

--- src/components/App.vue
+++ src/components/App.vue
@@ -15,13 +15,19 @@
         Checkout <code>./README.md</code> for more usages.
       </span>
     </div>
+    <p5></p5>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
+import p5 from "./p5.vue";
+
+export default {
+  name: "app",
+  components: {
+    p5: p5
   }
+};
 </script>
 
 <!-- CSS libraries -->
@@ -29,53 +35,59 @@
 
 <!-- Global CSS -->
 <style>
-  code {
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    color: #2c3e50;
-  }
+code {
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace, serif;
+  font-size: 0.9em;
+  white-space: pre-wrap;
+  color: #2c3e50;
+}
 
-  code::before, code::after {
-    content: '`';
-  }
+code::before,
+code::after {
+  content: "`";
+}
 </style>
 
 <!-- Scoped component css -->
 <!-- It only affect current component -->
 <style scoped>
-  #app {
-    text-align: center;
-  }
+#app {
+  text-align: center;
+}
 
-  #app h1 {
-    color: #2c3e50;
-    font-weight: 300;
-    margin: 0;
-  }
+#app h1 {
+  color: #2c3e50;
+  font-weight: 300;
+  margin: 0;
+}
 
-  .banner {
-    height: 200px;
-    background-color: #f6f6f6;
-    padding: 50px 10px;
-  }
+.banner {
+  height: 200px;
+  background-color: #f6f6f6;
+  padding: 50px 10px;
+}
 
-  .bottom {
-    padding: 80px 10px;
-    font-size: 24px;
-    font-weight: 300;
-  }
+.bottom {
+  padding: 80px 10px;
+  font-size: 24px;
+  font-weight: 300;
+}
 
-  .fade {
-    font-size: 14px;
-  }
+.fade {
+  font-size: 14px;
+}
 
-  .logo {
-    animation: spin 4s 1s infinite linear
-  }
+.logo {
+  animation: spin 4s 1s infinite linear;
+}
 
-  @keyframes spin {
-    from {transform:rotate(0deg);}
-    to {transform:rotate(360deg);}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
   }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
---
+++ src/components/p5.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="p5-container">
+    This is a p5 component!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vue-p5-component"
+};
+</script>
