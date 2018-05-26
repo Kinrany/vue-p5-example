1dc852319678dc1648a2693281dc7424643e600d
Ruslan <kinrany@yandex.com>
2018-05-26 19:50:48 +0300

    Rename p5-user component properties

--- src/components/p5-user.vue
+++ src/components/p5-user.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <button @click="p5_state.red = 0">Remove red color</button>
-    <p5 :draw="p5_draw" :state="p5_state" @update="p5_update" :setup="p5_setup"></p5>
+    <button @click="state.red = 0">Remove red color</button>
+    <p5 :draw="draw" :state="state" @update="update" :setup="setup"></p5>
   </div>
 </template>
 
@@ -15,13 +15,13 @@ export default {
   },
   data() {
     return {
-      p5_setup: sketch => {
+      setup: sketch => {
         sketch.createCanvas(300, 150);
       },
-      p5_draw: (sketch, state) => {
+      draw: (sketch, state) => {
         sketch.background(state.red, state.green, state.blue);
       },
-      p5_state: {
+      state: {
         red: 255,
         green: 200,
         blue: 0
@@ -29,10 +29,10 @@ export default {
     };
   },
   methods: {
-    p5_update() {
-      this.p5_state.blue += 3;
-      if (this.p5_state.blue > 255) {
-        this.p5_state.blue = 0;
+    update() {
+      this.state.blue += 3;
+      if (this.state.blue > 255) {
+        this.state.blue = 0;
       }
     }
   }
