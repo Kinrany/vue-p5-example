c709f155b2728c7d1d534cb13a615decfe84d353
Ruslan <kinrany@yandex.com>
2018-05-26 15:02:04 +0300

    Add a yellow square to test mounting p5

--- src/components/p5.vue
+++ src/components/p5.vue
@@ -1,11 +1,25 @@
 <template>
-  <div id="p5-container">
+  <div>
     This is a p5 component!
+    <div id="p5-container"></div>
   </div>
 </template>
 
 <script>
+import p5 from "p5";
+
 export default {
-  name: "vue-p5-component"
+  name: "vue-p5-component",
+  methods: {
+    draw(sketch) {
+      sketch.background(255, 200, 0);
+    }
+  },
+  mounted() {
+    new p5(sketch => {
+      sketch.setup = () => sketch.createCanvas(200, 200);
+      sketch.draw = () => this.draw(sketch);
+    }, "p5-container");
+  }
 };
 </script>
