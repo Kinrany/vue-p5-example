a760fa683262dbf75aadfa24aa7f0d1d8f4a7381
Ruslan <kinrany@yandex.com>
2018-05-30 22:20:26 +0300

    Update to vue-p5 Fix webpack build problem (replace webpack-command with webpack-cli)

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <p5 :state="state" 
-        :setup="setup" 
-        :draw="draw" 
-        @update="update" 
+    <p5 :setup="setup" 
+        :draw="draw"
         @key-pressed="keyPressed"
         @mouse-moved="mouseMoved"
         @mouse-dragged="mouseDragged">
@@ -29,32 +27,24 @@ export default {
   components: {
     p5: VueP5
   },
-  data() {
-    return {
-      setup: sketch => {
-        sketch.createCanvas(300, 150);
-      },
-      draw: (sketch, state) => {
-        const { red, green, blue, lines } = state;
-        sketch.background(red, green, blue);
-        for (let line of lines) {
-          sketch.stroke(line.color);
-          sketch.line(line.pmouseX, line.pmouseY, line.mouseX, line.mouseY);
-        }
-      },
-      state: {
-        red: 255,
-        green: 0,
-        blue: 0,
-        lines: []
-      }
-    };
-  },
+  data: () => ({
+    red: 255,
+    green: 0,
+    blue: 0,
+    lines: []
+  }),
   methods: {
-    update() {
-      this.state.blue += 3;
-      if (this.state.blue > 255) {
-        this.state.blue = 0;
+    setup(sketch) {
+      sketch.createCanvas(300, 150);
+    },
+    draw(sketch) {
+      this.blue = (this.blue + 3) % 255;
+
+      const { red, green, blue, lines } = this;
+      sketch.background(red, green, blue);
+      for (let line of lines) {
+        sketch.stroke(line.color);
+        sketch.line(line.pmouseX, line.pmouseY, line.mouseX, line.mouseY);
       }
     },
     keyPressed(keyCode) {
@@ -70,15 +60,15 @@ export default {
       this.pushLine({ mouseX, mouseY, pmouseX, pmouseY, color: 255 });
     },
     toggleRed() {
-      this.state.red = 255 - this.state.red;
+      this.red = 255 - this.red;
     },
     toggleGreen() {
-      this.state.green = 255 - this.state.green;
+      this.green = 255 - this.green;
     },
     pushLine(line) {
-      let lines = this.state.lines;
+      let lines = this.lines;
       lines.push(line);
-      this.state.lines = lines.slice(-100);
+      this.lines = lines.slice(-100);
     }
   }
 };
